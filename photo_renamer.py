ed82acdb94d7e5110a63b729be9229e5b254ff87
Johan Andersson <johan-andersson01@protonmail.com>
2019-01-05 19:07:57 +0100

    add support for .mov files and refactor some functions

--- photo_renamer.py
+++ photo_renamer.py
@@ -9,15 +9,17 @@ from shutil import copy, SameFileError
 import exifread
 from itertools import product
 from tqdm import tqdm
+from datetime import datetime
 
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--suffix',     type=str,   nargs='+',      default=['.png', '.jpg', '.raw'],  help='Filter on suffix')
-    parser.add_argument('--prefix',     type=str,   nargs='+',      default=[''],              help='Filter on prefix')
-    parser.add_argument('--exclude',    type=str,   nargs='+',                                 help='Exclude paths containing any of these strings')
-    parser.add_argument('--dir',        type=Path,  required=True,                             help='Working directory path')
-    parser.add_argument('--logfile',    type=Path,                                             help='Log file path')
+    parser.add_argument('--suffix',     type=str,   nargs='+',      default=['.png', '.jpg', '.raw', '.mov'],  help='Filter on suffix')
+    parser.add_argument('--prefix',     type=str,   nargs='+',      default=[''],                      help='Filter on prefix')
+    parser.add_argument('--exclude',    type=str,   nargs='+',                                         help='Exclude paths containing any of these strings')
+    parser.add_argument('--dir',        type=Path,  required=True,                                     help='Working directory path')
+    parser.add_argument('--out',        type=Path,  required=True,  default='./restructured',          help='Output  directory path')
+    parser.add_argument('--logfile',    type=Path,                                                     help='Log file path')
     return parser.parse_args()
 
 
@@ -25,17 +27,21 @@ def setup_log_file(filename):
     log.basicConfig(filename=filename, level=log.INFO, format='%(asctime)s %(message)s')
 
 
-def get_exif_tag(path: Path, tag:str='EXIF DateTimeOriginal'):
+def get_date(path: Path, exif_tag:str='EXIF DateTimeOriginal'):
     with open(path, 'rb') as img:
         tags = exifread.process_file(img)
     try:
-        return str(tags[tag])
+        return str(tags[exif_tag])
     except KeyError:
+        if 'MOV' in path.name:
+            # .mov files don't have exif tags but it seems
+            # that the original creation date is preserved in st_mtime
+            return str(datetime.fromtimestamp(int(path.stat().st_mtime)))
         return None
 
 
 def get_new_name(path: Path):
-    date = get_exif_tag(path)
+    date = get_date(path)
     if date:
         date = date.replace(' ', '_')
         date = date.replace(':', '.')
@@ -52,43 +58,38 @@ def prompt_proceed(msg='Proceed? (y/n)', exit=True):
     return True
 
 
-def rename_file(file_and_new_name: tuple, base_dir, new_dir='restructured', failed_dir='failed'):
-    new_dir = base_dir/Path(new_dir)
-    failed_dir = new_dir/Path(failed_dir)
+def create_dirs(base_dir, failed_dirname='failed'):
+    failed_dir = base_dir/Path(failed_dirname)
     try:
-        new_dir.mkdir()
+        base_dir.mkdir()
         failed_dir.mkdir()
     except FileExistsError:
         pass
+    return base_dir, failed_dir
 
-    file = file_and_new_name[0]
-    new_name = file_and_new_name[1]
-    if new_name: # if image has the requested exif tag
-        directory = new_dir/Path(new_name[:4])
-        if not directory.is_dir():
-            directory.mkdir()
-        try:
-            copy(file, directory)
-        except SameFileError:
-            log.info(f'copy for file {file} failed since it was already in the destination folder {directory}')
-            return
-        file_copy = directory/file.name
+
+def assert_different_dirs(base_dir, out_dir):
+    assert out_dir.parent != base_dir, 'Output directory should not be placed inside the directory being scanned'
+
+
+def rename_file(file, new_name, new_dir, failed_dir):
+    directory = new_dir/Path(new_name[:4]) if new_name else failed_dir/Path(file.parent.name)
+
+    if not directory.is_dir():
+        directory.mkdir()
+    try:
+        copy(file, directory)
+    except SameFileError:
+        log.info(f'copy for file {file} failed since it was already in the destination folder {directory}')
+    file_copy = directory/file.name
+    if new_name:
         file_copy.rename(directory/new_name)
-        log.info(f'copied {file.resolve()} -> {(directory/new_name).resolve()}')
-    else:
-        directory = failed_dir/Path(file.parent.name)
-        if not directory.is_dir():
-            directory.mkdir()
-        try:
-            copy(file, directory)
-        except SameFileError:
-            log.info(f'copy for file {file} failed since it was already in the destination folder {directory}')
-        file_copy = directory/file.name
-        log.info(f'copied {file.resolve()} -> {(directory/file).resolve()}')
+    log.info(f'copied {file.resolve()} -> {(directory/file).resolve()}')
 
 
 if __name__ == "__main__":
     args = get_args()
+    assert_different_dirs(args.dir, args.out)
     if args.logfile:
         setup_log_file(args.logfile)
     suffices = []
@@ -102,6 +103,7 @@ if __name__ == "__main__":
     if args.exclude:
         print(f'Ignoring all files in directories {args.exclude}')
     prompt_proceed()
+    new_dir, failed_dir = create_dirs(args.out)
 
     for prefix, suffix in tqdm(product(prefices, suffices)):
         for file in tqdm(glob.iglob(f'{args.dir}/**/{prefix}*{suffix}', recursive=True)):
@@ -109,5 +111,5 @@ if __name__ == "__main__":
             if file.is_dir():
                 continue
             if not args.exclude or args.exclude and not any([ex in str(file.parent.resolve()) for ex in args.exclude]):
-                file_and_new_name = (file, get_new_name(file))
-                rename_file(file_and_new_name, args.dir)
+                file, new_name = (file, get_new_name(file))
+                rename_file(file, new_name, new_dir, failed_dir)
